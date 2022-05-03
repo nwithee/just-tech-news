3cf3326cc17612518b0bc941ecd6f9ec4de355f7
nzwithee@gmail.com <Cartoon#01>
2022-05-03 18:14:23 -0500

    updates

---
+++ controllers/home-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+const { Post, User, Comment} = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+          'id',
+          'post_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: Comment,
+            //Removed created at because it was screwing with the code
+            attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+        .then(dbPostData => {
+          // pass a single post object into the homepage template
+          const posts = dbPostData.map(post => post.get ({ plain: true }));
+          res.render('homepage', { posts });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    });
+
+module.exports = router;
